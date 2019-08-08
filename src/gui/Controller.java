afe4875afcb2069d102cd3c8692fa9e0ec4b4d41
Aziz Sonawalla <sonawallaa@gmail.com>
2019-08-08 14:04:46 -0700

    Remove unused imports

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -13,8 +13,6 @@ import util.ImageUtil;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReadWriteLock;
